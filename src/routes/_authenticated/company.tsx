1d20896295745ba6ebe3f92a6024bcee9cec37a1
Yusufmukhi <yusufmukhi1@gmail.com>
2026-06-29 18:17:42 +0530

    a

--- src/routes/_authenticated/company.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
-import { Outlet } from "@tanstack/react-router";
-
-// Layout for /company/$symbol nested routes (currently just the index handled by company.$symbol.tsx).
-// Provided so report.tsx can also live under /_authenticated/company/$symbol/report.
-
-export const Route = createFileRoute("/_authenticated/company")({
-  component: () => <Outlet />,
-});
